d1bafc02e33c8dcc76c58753b22085ee937d800c
Ken Sipe <kensipe@gmail.com>
2008-10-19 13:46:20 -0500

    significant changes, core of real approach is there now.

--- JmxGrailsPlugin.groovy
+++ JmxGrailsPlugin.groovy
@@ -1,5 +1,9 @@
+import org.springframework.jmx.support.MBeanServerFactoryBean
+import org.springframework.jmx.export.MBeanExporter
+import org.hibernate.jmx.StatisticsService
+
 class JmxGrailsPlugin {
-    def version = 0.1
+    def version = 0.2
     def dependsOn = [:]
 
    
@@ -18,29 +22,58 @@ TODO: jmx jetty
     def documentation = "http://grails.org/JmxGrailsPlugin+Plugin"
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
+
+        // adding the mbean server configuration and export with mbeanserver ref... no exports
+        mbeanServer(MBeanServerFactoryBean) {
+            locateExistingServerIfPossible = true
+        }
+
+        hibernateStats(StatisticsService) {
+		    statisticsEnabled = true
+		    sessionFactory = ref("sessionFactory")
+	    }
+
+        exporter(MBeanExporter) {
+             server = ref(mbeanServer)
+             beans = [:]
+        }
+
     }
    
-    def doWithApplicationContext = { applicationContext ->
-      
+    def doWithApplicationContext = { ctx ->
+
+        // TODO add a bean to spring on the fly
+        // TODO discover services on the fly and register them with a convention
+        // TODO add a logging wrapper
+
+
+        println "****************** do with app ctx ****************"
+
+        MBeanExporter exporter = ctx.getBean("exporter")
+        def stats = ctx.getBean("hibernateStats")
+        def ds =ctx.getBean("dataSource")
+
+        exporter.beans."GrailsConfig:service=statistics,type=hibernate" = stats
+        exporter.beans."GrailsConfig:service=datasource" = ctx.dataSource
+
+        exporter.unregisterBeans()
+        exporter.registerBeans()
+
     }
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
+
     }
 	                                      
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
+
     }
 	
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
+
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
+
     }
 }
