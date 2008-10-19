366c54db885dbd58b57c86b4d556f69daa1bb088
Ken Sipe <kensipe@gmail.com>
2008-10-19 14:32:50 -0500

    Add Service discover based on static expose = ['jmx']

--- JmxGrailsPlugin.groovy
+++ JmxGrailsPlugin.groovy
@@ -2,6 +2,8 @@ import org.springframework.jmx.support.MBeanServerFactoryBean
 import org.springframework.jmx.export.MBeanExporter
 import org.hibernate.jmx.StatisticsService
 
+import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
+
 class JmxGrailsPlugin {
     def version = 0.2
     def dependsOn = [:]
@@ -28,11 +30,13 @@ TODO: jmx jetty
             locateExistingServerIfPossible = true
         }
 
+        // there is an mbean already defined with hibernate
         hibernateStats(StatisticsService) {
 		    statisticsEnabled = true
 		    sessionFactory = ref("sessionFactory")
 	    }
 
+        // configure the expo
         exporter(MBeanExporter) {
              server = ref(mbeanServer)
              beans = [:]
@@ -43,22 +47,40 @@ TODO: jmx jetty
     def doWithApplicationContext = { ctx ->
 
         // TODO add a bean to spring on the fly
-        // TODO discover services on the fly and register them with a convention
         // TODO add a logging wrapper
+        // TODO allow for static expose = ['jmx:ObjectName']
 
-
+        def configDomain = "GrailsConfig"
+        def appDomain = "GrailsApp"
         println "****************** do with app ctx ****************"
 
         MBeanExporter exporter = ctx.getBean("exporter")
-        def stats = ctx.getBean("hibernateStats")
-        def ds =ctx.getBean("dataSource")
+        exportConfigBeans(exporter, ctx, configDomain)
 
-        exporter.beans."GrailsConfig:service=statistics,type=hibernate" = stats
-        exporter.beans."GrailsConfig:service=datasource" = ctx.dataSource
+ 
+        application.serviceClasses?.each { service ->
+          def serviceClass = service.getClazz()
+          def serviceName = service.shortName
 
+          def exposeList = GCU.getStaticPropertyValue(serviceClass, 'expose')
+          if (exposeList != null && exposeList.contains('jmx')) {
+               // perhaps these should be limited to singletons
+              exporter.beans."${appDomain}:service=${serviceName}" = service
+          }
+        }
+
+        exportMBeans(exporter)
+
+    }
+
+    private def exportMBeans(MBeanExporter exporter) {
         exporter.unregisterBeans()
         exporter.registerBeans()
+    }
 
+    private def exportConfigBeans(MBeanExporter exporter, ctx, configDomain) {
+        exporter.beans."${configDomain}:service=statistics,type=hibernate" = ctx.hibernateStats
+        exporter.beans."${configDomain}:service=datasource" = ctx.dataSource
     }
 
     def doWithWebDescriptor = { xml ->
